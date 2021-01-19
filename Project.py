bc37ed72694b8b352b3135944a8df843086113c4
Veristor <74726907+Veristor@users.noreply.github.com>
2021-01-19 20:54:58 +0100

    wykresy, funkcje, usprawnienie menu

--- Project.py
+++ Project.py
@@ -1,3 +1,16 @@
+from typing import List
+
+import numpy as np
+import matplotlib.pyplot as plt
+vk=0
+vp=0
+F=0
+t=0
+m=0
+n=0
+g=0
+a=0
+
 print("""
                       ^^^^^^^^^^^^^^^^^^^^^^^^Program Ruch Zmienny^^^^^^^^^^^^^^^^^^^^^^^^
                                  Postępuj zgodnie z instrukcjami podanymi na ekranie
@@ -5,104 +18,333 @@ print("""
                                           Wybierz opcje 1 , 2 lub 3 
 """)
 opcje_menu = ["1 - Ruch z tarciem", "2 - Ruch bez tarcia", "3 - Zamknij Program"]
-menu1 = ["1 - Predkość początkowa (Vp)", "2 - Prędkość końcowa (Vk)","3 - Przyśpieszenie (a)","4 - Czas (t)","5 - Droga (s)","6 - Powrót do głównego menu"]
-menu2 = ["1 - Predkość początkowa (Vp)", "2 - Prędkość końcowa (Vk)","3 - Przyśpieszenie (a)","4 - Czas (t)","5 - Droga (s)","6 - Powrót do głównego menu"]
+menu1 = ["1 - Predkość początkowa (Vp) z wykresem V(t)", "2 - Prędkość końcowa (Vk) z wykresem V(t)","3 - Przyśpieszenie (a)","4 - Czas (t)","5 - Droga (s) z wykresem S(t)","6 - Powrót do głównego menu"]
+menu2 = ["1 - Predkość początkowa (Vp) z wykresem V(t)", "2 - Prędkość końcowa (Vk) z wykresem V(t)","3 - Przyśpieszenie (a)","4 - Czas (t)","5 - Droga (s) z wykresem S(t)","6 - Powrót do głównego menu"]
+
+
+
 #Funkcje do pierwszego wyboru
 
-def tarcie_vp(vk,F,t,m,n,g):
-    return vk -((F-n*m*g)/m)*t
+def tarcie_vp (vk, F, t, m, n, g):
+        Tsim = []
+        Vt = []
+        Vp = vk - ((F - n * m * g) / m)
+        for x in range(0, int(t * 10) + 1, 1):
+            Vt.append(Vp + ((F - n * m * g) / m) * (x / 10))
+            Tsim.append(float(x / 10))
+        print("Prędkość początkowa:", Vp, "m/s")
+        plt.plot(Tsim, Vt)
+        plt.xlabel("Czas[s]")
+        plt.ylabel("Predkosc[m/s]")
+        plt.title("Wykres Funkcji")
+        plt.show()
+        return Vp
 
 def tarcie_vk (vp,F,t,m,n,g):
-    return vp+((F-n*m*g)/m)*t
+    Tsim = []
+    Vt = []
+    Vk = vp+((F-n*m*g)/m)*t
+    for x in range(0, int(t * 10) + 1, 1):
+        Vt.append(Vk - ((F - n * m * g) / m) * (x / 10))
+        Tsim.append(float(x / 10))
+    print ("Prędkość końcowa:",Vk,"m/s")
+    plt.plot(Tsim, Vt)
+    plt.xlabel("Czas[s]")
+    plt.ylabel("Predkosc[m/s]")
+    plt.title("Wykres Funkcji")
+    plt.show()
+    return Vk
 
 def tarcie_a (m,F,g,n) :
     return (F-n*m*g)/m
 
-def tarcie_t (vp,vk,F,m,n,g) :
+def tarcie_t (vp,vk,F,m,n,g):
     return ((vk-vp)*m)/(F-n*m*g)
 
 def tarcie_s (vp,F,g,n,t,m) :
-    return vp*t+0,5*((F-n*m*g)/m)*t**2
+    Tsim = []
+    St = []
+    S= vp*t+0.5*((F-n*m*g)/m)*t**2
+    for x in range(0, int(t * 10) + 1, 1):
+        St.append((vp*x+0.5*((F-n*m*g)/m)*(x**2/10))/10)
+        Tsim.append(float(x/ 10))
+    print("Droga:", S, "m")
+    plt.plot(Tsim, St)
+    plt.xlabel("Czas[s]")
+    plt.ylabel("Droga [m]")
+    plt.title("Wykres Funkcji")
+    plt.show()
+    return S
 
-def get_inputs1(user_in1) :
+
+def get_inputs1(user_in1,vk,F,t,m,n,g):
     if user_in1 == '1':
-       vk = int(input("Podaj prędkośc końcową (Vk): "))
-       F = int(input("Podaj siłe ciągu "))
-       t = int(input("Podaj czas (t): "))
-       m = int(input("Podaj masę (m): "))
-       n = int(input("Podaj współczynnik tarcia (n): "))
-       g = int (input("Podaj przyśpieszenie ziemskie (g): "))
-       return tarcie_vp(vk,F,t,m,n,g)
+       while True:
+          try:
+                vk =float(input("Podaj prędkośc końcową (Vk): "))
+                F = float(input("Podaj siłe ciągu "))
+                if F<0:
+                    print("Liczba F musi być dodatnia")
+                    continue
+                t = float(input("Podaj czas (t): "))
+                if t<0:
+                    print("Liczba t musi być dodatnia")
+                    continue
+                m = float(input("Podaj masę (m): "))
+                if m<0:
+                    print("Liczba m musi być dodatnia")
+                    continue
+                n = float(input("Podaj współczynnik tarcia (n): "))
+                if n<0:
+                    print("Liczba n musi być dodatnia")
+                    continue
+                g = float(input("Podaj przyśpieszenie ziemskie (g): "))
+                if g<0:
+                    print("Liczba g musi być dodatnia")
+                    continue
+          except ValueError:
+              print("Musisz podać liczbę")
+              continue
+          else:
+              return tarcie_vp(vk,F,t,m,n,g)
+
     elif user_in1 == '2':
-       vp = int(input("Podaj prędkośc początkową (Vp): "))
-       F = int(input("Podaj siłe ciągu (F): "))
-       t = int(input("Podaj czas (t): "))
-       m = int(input("Podaj masę (m): "))
-       n = int(input("Podaj współczynnik tarcia (n): "))
-       g = int(input("Podaj przyśpieszenie ziemskie (g): "))
-       return tarcie_vk(vp,F,t,m,n,g)
+        while True:
+            try:
+               vp = float(input("Podaj prędkośc początkową (Vp): "))
+               if vp < 0:
+                   print("Liczba vp musi być dodatnia")
+                   continue
+               F = float(input("Podaj siłe ciągu (F): "))
+               t = float(input("Podaj czas (t): "))
+               if t < 0:
+                   print("Liczba t musi być dodatnia")
+                   continue
+               m = float(input("Podaj masę (m): "))
+               if m < 0:
+                   print("Liczba m musi być dodatnia")
+                   continue
+               n = float(input("Podaj współczynnik tarcia (n): "))
+               if n < 0:
+                   print("Liczba n musi być dodatnia")
+                   continue
+               g = float(input("Podaj przyśpieszenie ziemskie (g): "))
+               if g < 0:
+                   print("Liczba g musi być dodatnia")
+                   continue
+            except ValueError:
+                print("Musisz podać liczbę")
+                continue
+            else:
+               return tarcie_vk(vp,F,t,m,n,g)
     elif user_in1 == '3':
-       F = int(input("Podaj siłe ciągu (F): "))
-       m = int(input("Podaj masę (m): "))
-       n = int(input("Podaj współczynnik tarcia (n): "))
-       g = int(input("Podaj przyśpieszenie ziemskie (g): "))
-       return tarcie_a(F,g,m,n)
+        while True:
+            try:
+               F = float(input("Podaj siłe ciągu (F): "))
+               m = float(input("Podaj masę (m): "))
+               if m < 0:
+                   print("Liczba m musi być dodatnia")
+                   continue
+               n = float(input("Podaj współczynnik tarcia (n): "))
+               if n < 0:
+                   print("Liczba n musi być dodatnia")
+                   continue
+               g = float(input("Podaj przyśpieszenie ziemskie (g): "))
+               if g < 0:
+                   print("Liczba g musi być dodatnia")
+                   continue
+            except ValueError:
+                print("Musisz podać liczbę")
+                continue
+            else:
+                return tarcie_a(F,g,m,n)
     elif user_in1 == '4':
-       vp = int(input("Podaj prędkośc początkową (Vp): "))
-       F = int(input("Podaj siłe ciągu (F): "))
-       vk = int(input("Podaj prędkośc końcową (Vk): "))
-       m = int(input("Podaj masę (m): "))
-       n = int(input("Podaj współczynnik tarcia (n): "))
-       g = int(input("Podaj przyśpieszenie ziemskie (g): "))
-       return tarcie_t(vp,vk,F,m,n,g)
+        while True:
+            try:
+                   vp = float(input("Podaj prędkośc początkową (Vp): "))
+                   if vp < 0:
+                       print("Liczba vp musi być dodatnia")
+                       continue
+                   F = float(input("Podaj siłe ciągu (F): "))
+                   vk = float(input("Podaj prędkośc końcową (Vk): "))
+                   m = float(input("Podaj masę (m): "))
+                   if m < 0:
+                       print("Liczba m musi być dodatnia")
+                       continue
+                   n = float(input("Podaj współczynnik tarcia (n): "))
+                   if n < 0:
+                       print("Liczba n musi być dodatnia")
+                       continue
+                   g = float(input("Podaj przyśpieszenie ziemskie (g): "))
+                   if g < 0:
+                       print("Liczba g musi być dodatnia")
+                       continue
+            except ValueError:
+                print("Musisz podać liczbę")
+                continue
+            else:
+             return tarcie_t,(vp,vk,F,m,n,g)
     elif user_in1 == '5':
-       vp = int(input("Podaj prędkośc początkową (Vp): "))
-       F = int(input("Podaj siłe ciągu (F): "))
-       t = int(input("Podaj czas (t): "))
-       m = int(input("Podaj masę (m): "))
-       n = int(input("Podaj współczynnik tarcia (n): "))
-       g = int(input("Podaj przyśpieszenie ziemskie (g): "))
-       return tarcie_s(vp,F,t,m,n,g)
+      while True:
+          try:
+               vp = float(input("Podaj prędkośc początkową (Vp): "))
+               if vp < 0:
+                   print("Liczba vp musi być dodatnia")
+                   continue
+               F = float(input("Podaj siłe ciągu (F): "))
+               t = float(input("Podaj czas (t): "))
+               if t < 0:
+                   print("Liczba musi t być dodatnia")
+                   continue
+               m = float(input("Podaj masę (m): "))
+               if m < 0:
+                   print("Liczba musi m być dodatnia")
+                   continue
+               n = float(input("Podaj współczynnik tarcia (n): "))
+               if n < 0:
+                   print("Liczba musi n być dodatnia")
+                   continue
+               g = float(input("Podaj przyśpieszenie ziemskie (g): "))
+               if g < 0:
+                   print("Liczba musi g być dodatnia")
+                   continue
+          except ValueError:
+              print("Musisz podać liczbę")
+              continue
+          else:
+            return tarcie_s(vp,F,t,m,n,g)
 #Funkcje do drugiego wyboru
 
 
 def vp2(vk, a, t):
-    return vk - a*t
+    Tsim=[]
+    Vt=[]
+    Vp = vk - a*t
+    for x in range(0,int(t*10)+1,1):
+        Vt.append(Vp + a*(x/10))
+        Tsim.append(float(x/10))
+    print("Prędkość początkowa:", Vp, "m/s")
+    plt.plot(Tsim,Vt)
+    plt.xlabel("Czas[s]")
+    plt.ylabel("Predkosc[m/s]")
+    plt.title("Wykres Funkcji")
+    plt.show()
+    return Vp
+
 def vk2 (vp,a,t):
-    return vp + a*t
+    Tsim = []
+    Vt = []
+    Vk = vp + a*t
+    for x in range(0, int(t * 10) + 1, 1):
+        Vt.append(Vk - a * (x / 10))
+        Tsim.append(float(x / 10))
+    print("Prędkość końcowa:", Vk, "m/s")
+    plt.plot(Tsim, Vt)
+    plt.xlabel("Czas[s]")
+    plt.ylabel("Predkosc[m/s]")
+    plt.title("Wykres Funkcji")
+    plt.show()
+    return Vk
 def a2 (vp,vk,t) :
     return (vk - vp)/t
 def t2 (vp,vk,a):
     return (vk - vp)/a
 def s2 (vp,a,t):
-    return vp + vp*t + 0,5*a*t**2
+    Tsim = []
+    St = []
+    S = vp + vp*t + 0.5*a*t**2
+    for x in range(0, int(t * 10) + 1, 1):
+        St.append(vp + vp*x + 0.5*a*(x ** 2 / 10))
+        Tsim.append(float(x / 10))
+    print("Droga:", S, "m")
+    plt.plot(Tsim, St)
+    plt.xlabel("Czas[s]")
+    plt.ylabel("Droga [m]")
+    plt.title("Wykres Funkcji")
+    plt.show()
+    return S
+
 
 def get_inputs2(user_in2) :
     if user_in2 == '1':
-       vk = int(input("Podaj prędkośc końcową (Vk): "))
-       a = int(input("Podaj przyśpieszenie (a): "))
-       t = int(input("Podaj czas (t): "))
-       return vp2(vk,a,t)
+      while True:
+          try:
+                vk = float(input("Podaj prędkośc końcową (Vk): "))
+                a = float(input("Podaj przyśpieszenie (a): "))
+                t = float(input("Podaj czas (t): "))
+                if t < 0:
+                      print("Liczba musi t być dodatnia")
+                      continue
+          except ValueError:
+              print("Musisz podać liczbę")
+              continue
+          else:
+            return vp2(vk,a,t)
     elif user_in2 == '2':
-       vp = int(input("Podaj prędkośc początkową (Vp): "))
-       a = int(input("Podaj przyśpieszenie (a): "))
-       t = int(input("Podaj czas (t): "))
-       return vk2(vp,a,t)
+        while True:
+            try:
+                   vp = float(input("Podaj prędkośc początkową (Vp): "))
+                   if vp < 0:
+                       print("Liczba vp musi być dodatnia")
+                       continue
+                   a = float(input("Podaj przyśpieszenie (a): "))
+                   t = float(input("Podaj czas (t): "))
+                   if t < 0:
+                       print("Liczba musi t być dodatnia")
+                       continue
+            except ValueError:
+                print("Musisz podać liczbę")
+                continue
+            else:
+               return vk2(vp,a,t)
     elif user_in2 == '3':
-       vp = int(input("Podaj prędkośc początkową (Vp): "))
-       vk = int(input("Podaj prędkośc końcową (Vk): "))
-       t = int(input("Podaj czas (t): "))
-       return a2(vp,vk,t)
+        while True:
+            try:
+
+                   vp = float(input("Podaj prędkośc początkową (Vp): "))
+                   if vp < 0:
+                       print("Liczba vp musi być dodatnia")
+                       continue
+                   vk = float(input("Podaj prędkośc końcową (Vk): "))
+                   t = float(input("Podaj czas (t): "))
+                   if t < 0:
+                       print("Liczba musi t być dodatnia")
+                       continue
+            except ValueError:
+                print("Musisz podać liczbę")
+                continue
+            else:
+                return a2(vp,vk,t)
     elif user_in2 == '4':
-       vp = int(input("Podaj prędkośc początkową (Vp): "))
-       a = int(input("Podaj przyśpieszenie (a): "))
-       vk = int(input("Podaj prędkośc końcową (Vk): "))
-       return t2(vp,vk,a)
-    elif user_in2 == '4':
-       vp = int(input("Podaj prędkośc początkową (Vp): "))
-       a = int(input("Podaj przyśpieszenie (a): "))
-       t = int(input("Podaj czas (t): "))
-       return s2(vp,a,t)
+        while True:
+            try:
+               vp = float(input("Podaj prędkośc początkową (Vp): "))
+               if vp < 0:
+                   print("Liczba vp musi być dodatnia")
+                   continue
+               a = float(input("Podaj przyśpieszenie (a): "))
+               vk = float(input("Podaj prędkośc końcową (Vk): "))
+            except ValueError:
+                continue
+            else:
+                return t2(vp,vk,a)
+    elif user_in2 == '5':
+        while True:
+            try:
+                   vp = float(input("Podaj prędkośc początkową (Vp): "))
+                   if vp < 0:
+                       print("Liczba vp musi  być dodatnia")
+                       continue
+                   a = float(input("Podaj przyśpieszenie (a): "))
+                   t = float(input("Podaj czas (t): "))
+                   if t < 0:
+                       print("Liczba t musi być dodatnia")
+                       continue
+            except ValueError:
+                    print ("Musisz podać liczbę")
+            else:
+                return s2(vp,a,t)
+
 
 
 while True:
@@ -120,21 +362,17 @@ while True:
                          "")
 ####################
         if user_in1 == "1":
-            print("Aby wyliczyć prędkość początkową potrzebne są: predkość końcowa, przyśpieszenie oraz czas\n ")
-            vp = get_inputs1(user_in1)
-            print("Prękośc początkowa :", vp)
+            vp = get_inputs1(user_in1,vk,F,t,m,n,g)
         elif user_in1 == "2":
-            vk = get_inputs1(user_in1)
-            print("Prędkośc końcowa :", vk)
+            vk = get_inputs1(user_in1,vk,F,t,m,n,g)
         elif user_in1 == "3":
-            a = get_inputs1(user_in1)
-            print("Przyśpieszenie :", a,)
+            a = get_inputs1(user_in1,vk,F,t,m,n,g)
+            print("Przyśpieszenie:", a, "m/s**2")
         elif user_in1 == "4":
-            t = get_inputs1(user_in1)
-            print("Czas :", t)
+            t = get_inputs1(user_in1,vk,F,t,m,n,g)
+            print("Czas:", tarcie_t, "s")
         elif user_in1 == "5":
-            s = get_inputs1(user_in1)
-            print("Droga :", s)
+            s = get_inputs1(user_in1,vk,F,t,m,n,g)
         elif user_in1 == "6":
             continue
         else:
@@ -151,21 +389,17 @@ while True:
                          "")
         ####################
         if user_in2 == "1":
-            print("Aby wyliczyć prędkość początkową potrzebne są: predkość końcowa, przyśpieszenie oraz czas\n ")
             vp2 = get_inputs2(user_in2)
-            print("Prękośc początkowa :", vp2)
         elif user_in2 == "2":
             vk2 = get_inputs2(user_in2)
-            print("Prędkośc końcowa :", vk2)
         elif user_in2 == "3":
             a2 = get_inputs2(user_in2)
-            print("Przyśpieszenie :", a2)
+            print("Przyśpieszenie:", a2, "m/s**2")
         elif user_in2 == "4":
             t2 = get_inputs2(user_in2)
-            print("Czas :", t2)
+            print("Czas:", t2, "s")
         elif user_in2 == "5":
             s2 = get_inputs2(user_in2)
-            print("Droga :", s2)
         elif user_in2 == "6":
             continue
         else:
